182f5be5e7d23f39b55e475bb2786c85c622bcd8
Demmyyoung <demiladepc1@gmail.com>
2026-05-31 20:00:35 +0100

    feat: implement responsive Contact page component with animation and contact details

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -38,11 +38,7 @@ const contacts = [
     external: false,
   },
   {
-<<<<<<< HEAD
     href: "https://www.linkedin.com/in/uchechukwu-ogbuaku-1a6145306/",
-=======
-    href: "https://www.linkedin.com/in/uchechukwu-ogbuaku-1a6145306?utm_source=share_via&utm_content=profile&utm_medium=member_ios",
->>>>>>> bfa68127b66c4c5f5bc680061e7bbe998235bc62
     icon: Linkedin,
     label: "LinkedIn",
     display: "linkedin.com/in/uchechukwu-ogbuaku-1a6145306",
