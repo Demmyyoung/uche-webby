29dbf675dcb3bd5c27ab6ba70087f617b0536e5c
demmy <112753184+Demmyyoung@users.noreply.github.com>
2026-05-31 18:39:40 +0100

    Update LinkedIn profile URL

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -38,7 +38,7 @@ const contacts = [
     external: false,
   },
   {
-    href: "https://www.linkedin.com/in/uchechukwu-ogbuaku-1a6145306?utm_source=share_via&utm_content=profile&utm_medium=member_android",
+    href: "https://www.linkedin.com/in/uchechukwu-ogbuaku-1a6145306",
     icon: Linkedin,
     label: "LinkedIn",
     display: "linkedin.com/in/uchechukwu-ogbuaku-1a6145306",
